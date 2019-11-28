46746b7ff687bb9262f8575081a3d8edafe608e4
Vinh Le <vinh@vertics.co>
2019-11-28 06:33:22 +0200

    feat: add selection sort

--- sort/v-sort/index.js
+++ sort/v-sort/index.js
@@ -22,5 +22,26 @@ const bubbleSort = (arr) => {
 	return arr
 }
 
-const array = [6,2,1,7,3,12]
-console.log(bubbleSort(array))
+const selectionSort = (arr) => {
+	for(let i = 0; i <= arr.length; i ++) {
+		// set minimum to this position
+		let min = i
+		for(let j = i+1; j <= arr.length; j++) {
+			// find the min value
+			if(arr[j] < arr[min]) {
+				min = j
+			}
+		}
+
+		// check if min is actually the first item
+		if(i !== min) {
+			swap(arr, i, min)
+		}
+	}
+
+	return arr
+}
+
+const array = [100, 4, 0, 18, 9, 6,2,1,7,3,12]
+// console.log(bubbleSort(array))
+console.log(selectionSort(array))
